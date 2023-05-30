94a15bbd1e90b41e959a54ffe881475502326573
kropli <dimakirimail@gmail.com>
2023-05-30 18:02:53 +0300

    Added models for Code First approach

---
+++ Models/Spending.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    public class Spending
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal SpendingAmount { get; set; }
+    }
+}
